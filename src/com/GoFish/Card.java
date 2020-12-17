7f58f7b7863bba85a8234418aa9c262ac98bb175
David Jones <coreldavid@hotmail.com>
2020-12-16 21:38:19 -0500

    card class created

---
+++ src/com/GoFish/Card.java
@@ -0,0 +1,19 @@
+package com.GoFish;
+
+public class Card {
+    protected String cardSuit;
+    protected String cardRank;
+
+    public Card(String newSuit, String newRank) {
+        this.cardSuit = newSuit;
+        this.cardRank = newRank;
+    }
+
+    public String getCardSuit() {
+        return cardSuit;
+    }
+
+    public String getCardRank() {
+        return cardRank;
+    }
+}
